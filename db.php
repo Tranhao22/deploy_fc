4a584a35ba2f2b2bca2b7826555754a045c476bf
Hào Trần <67888938+Tranhao22@users.noreply.github.com>
2021-12-18 17:31:39 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 	function query($sql){
 		//ket noi server
-		$conn=mysqli_connect("mysql5037.site4now.net","a7c75f_haoit22","Tranvanhao221220@") or die("lỗi kết nối");
+		$conn=mysqli_connect("mysql5037.site4now.net","a7c75f_haoit22","Admin1234") or die("lỗi kết nối");
 		mysqli_select_db($conn,"db_a7c75f_haoit22") or die("lỗi csdl");
 		//thuc hiện truy vấn
 		$result=mysqli_query($conn,$sql) or die("lỗi truy vấn");
